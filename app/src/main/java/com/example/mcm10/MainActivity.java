30db2cfdd4c576f5dea1dae5a5ce5993fe023c16
matajeric <matajeric2006@gmail.com>
2024-05-20 14:44:12 +0800

    Commit

--- app/src/main/java/com/example/mcm10/MainActivity.java
+++ app/src/main/java/com/example/mcm10/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadFragment(new HomeFragment(), false);
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         frameLayout = findViewById(R.id.frameLayout);
 
